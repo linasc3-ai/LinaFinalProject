d819cf7745bd87c1b2df461c8fa8631665e3ffd4
Lina Chihoub <lchihoub@sas.upenn.edu>
2024-04-25 11:00:55 -0400

    Initial Commit

---
+++ LinaFinalProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinaFinalProject
+//
+//  Created by Lina Chihoub on 4/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinaFinalProject/LinaFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinaFinalProjectApp.swift
+//  LinaFinalProject
+//
+//  Created by Lina Chihoub on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LinaFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
